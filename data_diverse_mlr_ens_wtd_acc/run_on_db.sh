7248ce5455579b2deab9bd17024f574531e45dba
guptarah <guptarah@usc.edu>
2013-11-19 12:02:00 -0800

    committing the directory with SA on acc and GA on wtd ll

---
+++ data_diverse_mlr_ens_wtd_acc/run_on_db.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+database_dir=$1
+
+for (( c=1; c<=100; c++))
+do
+	echo $database_dir
+	python train_ens_w_sa.py $database_dir 2
+	python train_ens_w_sa.py $database_dir .99
+#	python train_ens_w_sa_on_dev.py $database_dir 2
+#	python train_ens_w_sa_on_dev.py $database_dir .99
+	rm $database_dir'cv_cur' $database_dir/new_wts $database_dir/weight_file $database_dir/new_wts_dev
+done
